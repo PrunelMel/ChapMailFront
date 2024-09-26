79522eefdc02610c35ef9f41e6340191dee80120
Prunel Mel <seyive.awaya@gmail.com>
2024-09-26 20:32:59 +0100

    removing data

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,24 +4,26 @@ import EmailCard from '../components/EmailCard';
 const Dashboard = () => {
 
   const emails = [
-    {
-      id: 1,
-      sender: 'John Doe',
-      subject: 'Welcome to Anonymous Mail',
-      snippet: 'Thank you for signing up to our platform...',
-      date: 'Sep 17',
-      isRead: false,
-    },
-    {
-      id: 2,
-      sender: 'Jane Smith',
-      subject: 'Your Account Has Been Updated',
-      snippet: 'We have updated your account settings...',
-      date: 'Sep 16',
-      isRead: true,
-    },
-    // Ajoute d'autres emails ici
+    
   ];
+    // {
+    //   id: 1,
+    //   sender: 'John Doe',
+    //   subject: 'Welcome to Anonymous Mail',
+    //   snippet: 'Thank you for signing up to our platform...',
+    //   date: 'Sep 17',
+    //   isRead: false,
+    // },
+    // {
+    //   id: 2,
+    //   sender: 'Jane Smith',
+    //   subject: 'Your Account Has Been Updated',
+    //   snippet: 'We have updated your account settings...',
+    //   date: 'Sep 16',
+    //   isRead: true,
+    // },
+    // Ajoute d'autres emails ici
+  
   return (
     <div>
       <Header />
